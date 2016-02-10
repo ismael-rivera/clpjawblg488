878e0e72b68ccb49694b0a007395ebdfeca97d07
Ismael Ishy Rivera <coralblue79@gmail.com>
2016-02-10 03:15:03 -0200

    first awe file render commit

---
+++ footer.php
@@ -0,0 +1,42 @@
+<footer class="footer tborder">
+		<div class="spacer45"></div>
+        <div class="container">
+        	<div class="row">
+        		<div class="col-sm-3"></div>
+          		<div class="col-sm-3">
+          			<ul>
+          				<li>link</li>
+          				<li>link</li>
+          				<li>link</li>
+          				<li>link</li>
+          			</ul>
+          		</div>
+          		<div class="col-sm-3">
+          			<ul>
+          				<li>link</li>
+          				<li>link</li>
+          				<li>link</li>
+          				<li>link</li>
+          				<li>link</li>
+          			</ul>
+          		</div>
+          		<div class="col-sm-3">
+          			<ul>
+          				<li>link</li>
+          				<li>link</li>
+          				<li>link</li>
+          				<li>link</li>
+          				<li>link</li>
+          			</ul>
+          		</div>
+      		</div>
+      	</div>
+      	<div class="spacer"></div> 
+      	<div class="container">
+        	<div class="row">
+          		<div class="col-sm-4"><p class="text-muted">© 2015 Company, Inc.</p></div>
+          		<div class="col-sm-4">ads</div>
+          		<div class="col-sm-4">ads</div>
+      		</div>
+      	</div>
+</footer>
---
+++ index.php
@@ -0,0 +1,109 @@
+<?php Starkers_Utilities::get_template_parts( array( 'html_header', 'header' ) ); ?>
+
+    <div id="featured" class="container fborder">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div id="feat1" class="col-sm-4">
+          <h2>Heading 1</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="http://getbootstrap.com/examples/jumbotron/#" role="button">View details »</a></p>
+        </div>
+        <div class="col-sm-8 lborder">
+            <div class="row">
+              <div id="feat2" class="col-sm-12">  
+                <h2>Heading 2</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-default" href="http://getbootstrap.com/examples/jumbotron/#" role="button">View details »</a></p>
+              </div>  
+            </div>
+            <div class="row">
+              <div id="masthead-ad" class="col-sm-6 tborder">  
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-default" href="http://getbootstrap.com/examples/jumbotron/#" role="button">View details »</a></p>
+              </div>
+              <div id="feat4" class="col-sm-6 tborder lborder">  
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-default" href="http://getbootstrap.com/examples/jumbotron/#" role="button">View details »</a></p>
+              </div>   
+            </div>   
+        </div>
+      </div>
+    </div>
+    <div class="spacer"></div>  
+    <div id="cap-ads-section" class="container fborder">
+      <div class="row">
+          <div class="col-sm-4 fborder">H</div>
+          <div class="col-sm-4 fborder">E</div>
+          <div class="col-sm-4 fborder">LLO</div>
+      </div>
+    </div>
+    <div class="spacer"></div> 
+    <div id="content-section" class="container fborder">
+        <div class="row">
+              <div id="content" class="col-sm-8 rborder">
+
+
+                <!-- article-web-component BEGIN -->
+                <div class="row">
+                  <div id="article_pic" class="col-sm-4"></div>
+                  <div id="article_link" class="col-sm-8 lborder">
+                    <h2><a class="article_link_title" href="http://getbootstrap.com/examples/blog/#">Home Gets Evaporated in Front of Owners</a></h2>
+                    <p class="excerpt">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                  </div>
+                </div>
+                <!-- article-web-component END -->
+                <div class="row">
+                  <div id="article_pic" class="col-sm-4"></div>
+                  <div id="article_link" class="col-sm-8 lborder">
+                    <h2><a class="article_link_title" href="http://getbootstrap.com/examples/blog/#">Home Gets Evaporated in Front of Owners</a></h2>
+                    <p class="excerpt">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                  </div>
+                </div>
+                <!-- article-web-component BEGIN -->
+                <div class="row">
+                  <div id="article_pic" class="col-sm-4"></div>
+                  <div id="article_link" class="col-sm-8 lborder">
+                    <h2><a class="article_link_title" href="http://getbootstrap.com/examples/blog/#">Home Gets Evaporated in Front of Owners</a></h2>
+                    <p class="excerpt">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                  </div>
+                </div>
+                <!-- article-web-component END -->
+                <!-- article-web-component BEGIN -->
+                <div class="row">
+                  <div id="article_pic" class="col-sm-4"></div>
+                  <div id="article_link" class="col-sm-8 lborder">
+                    <h2><a class="article_link_title" href="http://getbootstrap.com/examples/blog/#">Home Gets Evaporated in Front of Owners</a></h2>
+                    <p class="excerpt">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                  </div>
+                </div>
+                <!-- article-web-component END -->
+                <!-- article-web-component BEGIN -->
+                <div class="row">
+                  <div id="article_pic" class="col-sm-4"></div>
+                  <div id="article_link" class="col-sm-8 lborder">
+                    <h2><a class="article_link_title" href="http://getbootstrap.com/examples/blog/#">Home Gets Evaporated in Front of Owners</a></h2>
+                    <p class="excerpt">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                  </div>
+                </div>
+                <!-- article-web-component END -->
+
+
+              </div>
+              <div id="sidebar" class="col-sm-4">
+              </div>
+        </div> 
+    </div>
+
+    <div class="spacer"></div> 
+
+    <div id="ads" class="container">
+      <div class="col-sm-4 fborder">ads</div>
+      <div class="col-sm-4 fborder">ads</div>
+      <div class="col-sm-4 fborder">ads</div>
+    </div>
+
+    <div class="spacer"></div> 
+
+<?php Starkers_Utilities::get_template_parts( array( 'footer','html_footer') ); ?>
