0e53906f0d1853b4f7d0fd8a6552a0d05a5b3f5f
Ismael Ishy Rivera <coralblue79@gmail.com>
2016-02-22 07:06:51 -0300

    more of the same

---
+++ assets/js/site.js
@@ -0,0 +1,7 @@
+if (typeof jQuery == 'undefined') {  
+    // jQuery is not loaded 
+	alert('jQuery is not loaded');
+} else {
+    // jQuery is loaded
+	alert('jQuery is loaded');
+} 
--- html_header.php
+++ html_header.php
@@ -28,6 +28,8 @@
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="assets/js/libs/ie-emulation-modes-warning.js"></script>
 
+        <script src="assets/js/libs/jquery.slicknav.min.js"></script>
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
